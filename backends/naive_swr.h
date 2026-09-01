42e26603c515636d026ff574e43efda34d211cfe
MouriNaruto <Kenji.Mouri@outlook.com>
2026-09-02 00:32:09 +0800

    Add naive_swr.h.

---
+++ backends/naive_swr.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:    imgui-framebuffer
+ * FILE:       naive_swr.h
+ * PURPOSE:    Definition for the Naive Software Renderer
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NAIVE_SWR
+#define NAIVE_SWR
+
+#include <stdint.h>
+
+#endif // !NAIVE_SWR
