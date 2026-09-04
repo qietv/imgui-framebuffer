cdcf633a6057dadb31415076a12881e2e5c68317
MouriNaruto <Kenji.Mouri@outlook.com>
2026-09-04 19:03:08 +0800

    Create backends/naive_swr.c.

---
+++ backends/naive_swr.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    imgui-framebuffer
+ * FILE:       naive_swr.c
+ * PURPOSE:    Implementation for the Naive Software Renderer
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "naive_swr.h"
